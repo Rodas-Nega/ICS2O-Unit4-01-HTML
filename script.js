1fa2ae4d3771a67c312f9adde39a773667c3deea
RodasNega <rodas.nega@ocsbstudent.ca>
2020-12-08 21:03:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello, world!");
+
+function french() {
+  document.getElementById("lang1").style.textAlign = "left";
+
+function english() {
+  document.getElementById("lang2").style.textAlign = "center";
+}
+
+function japanese() {
+  document.getElementById("lang3").style.textAlign = "right";
+}
+
